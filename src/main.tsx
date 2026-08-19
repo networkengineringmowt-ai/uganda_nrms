7c7fc008edfea5254b65d0038addc69197a76610
priscananjehe1996 <priscananjehe@gmail.com>
2026-08-19 21:02:05 +0300

    Import table enhancements CSS for styling

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/transitions.css'
+import './styles/table-enhancements.css'
 import './mobile.css'
 import App from './App.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
